820cdea61ced50cf20e1f5065f58b592b7e0ddbf
Jakob <shavinski.jakob@gmail.com>
2023-05-09 15:34:24 -0700

    did nothing

--- markov.js
+++ markov.js
@@ -1,4 +1,5 @@
 /** Textual markov chain generator. */
+"use strict"
 
 
 class MarkovMachine {
@@ -28,11 +29,34 @@ class MarkovMachine {
 
   getChains() {
     // TODO: implement this!
+
+
+    // 
+    // const chain = {
+    //   "Women": ["and", "and"],
+    //   "and": ["men", "small"],
+    //   "men": ["both"],
+    //   "both": ["little"],
+    //   "little": ["and"],
+    //   "small": ["cared"],
+    //   "cared": ["for"],
+    //   "for": ["anyone"],
+    //   "anyone": ["not"],
+    //   "not": ["at"]
+
+    // }
+
+
   }
 
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
+
+  // Women and men both little and small
+  // cared for anyone not at all
+  // they sowed their isn't they reaped their same
+  // sun moon stars rain
 
   getText() {
     // TODO: implement this!
@@ -42,3 +66,7 @@ class MarkovMachine {
     // - repeat until reaching the terminal null
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
