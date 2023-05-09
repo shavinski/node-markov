ec0b575c16a70e76f93363b14232ee3681029eb6
Xiandi Pan <cindypan1807@gmail.com>
2023-05-09 19:10:22 -0400

    getText() init

--- markov.js
+++ markov.js
@@ -9,14 +9,16 @@ class MarkovMachine {
   constructor(text) {
     // A "word" will also include any punctuation around the word, so this will
     // include things like "The", "cat", "cat.".
+
     this.words = text.split(/[ \r\n]+/);
+    console.log(this.words)
     this.chains = this.getChains();
   }
 
   /** Get markov chain: returns object of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -24,42 +26,38 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
     // TODO: implement this!
+    const chainWords = {};
 
-
-    // 
-    // const chain = {
-    //   "Women": ["and", "and"],
-    //   "and": ["men", "small"],
-    //   "men": ["both"],
-    //   "both": ["little"],
-    //   "little": ["and"],
-    //   "small": ["cared"],
-    //   "cared": ["for"],
-    //   "for": ["anyone"],
-    //   "anyone": ["not"],
-    //   "not": ["at"]
-
-    // }
-
+    for (let i = 0; i < this.words.length; i++){
+      console.log(this.words[i]);
+      if ((this.words[i]) in chainWords){
+        chainWords[this.words[i]].push(this.words[i+1])
+      }else{
+        chainWords[this.words[i]] = [this.words[i+1]]
+      }
+    }
+    console.log(chainWords)
+    return chainWords;
 
   }
 
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
+  getRandomWord(words){
+    return Math.floor(Math.random() * words.length)
+  }
 
-  // Women and men both little and small
-  // cared for anyone not at all
-  // they sowed their isn't they reaped their same
-  // sun moon stars rain
+
 
   getText() {
     // TODO: implement this!
+    
 
     // - start at the first word in the input text
     // - find a random word from the following-words of that
